4996d862c371bf304d18a4c727d803e86a2bf029
Shweta Sinha <sinhashweta713@gmail.com>
2024-05-27 16:56:31 +0530

    max element in array

---
+++ src/Array/maxElem.java
@@ -0,0 +1,14 @@
+package Array;
+
+public class maxElem {
+    public static int findMax(int[] array){
+        int max = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] > max) {
+                max = array[i];
+            }
+        }
+        return max;
+
+    }
+}
